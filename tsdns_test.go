93810f4e624c17d53766d05ac72cc267cd1d1072
Johannes Waigel <support@allesverhext.de>
2018-07-08 14:59:23 +0200

    fix TSDNS description
    * description
    + add reqLen

--- tsdns_test.go
+++ tsdns_test.go
@@ -1,13 +1,13 @@
 package goTSDNS_test
 
 import (
-    "log"
-    "testing"
-    "github.com/Freaxbot/TSDNS"
+	"log"
+	"testing"
+	"github.com/Freaxbot/TSDNS"
 )
 
 func TestLookup(t *testing.T) {
-    res :=goTSDNS.Lookup("public.teamspeak.com", "127.0.0.1")
-    log.Println(("TSDNS :: " + res))
+	res :=goTSDNS.Lookup("public.teamspeak.com", "127.0.0.1")
+	log.Println("TSDNS :: " + res)
 
 }
